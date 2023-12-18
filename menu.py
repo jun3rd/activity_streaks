9f8835843a72b985fa7e5ea2d33a8201eda33d0c
alvarado <alvarado@Khaias-MacBook-Air.local>
2023-12-18 13:37:58 -0800

    initial python files

---
+++ menu.py
@@ -0,0 +1,11 @@
+def showmenu():
+    print("Activities:")
+    print("(1) START coding python: activity_streaks")
+    print("(2) END coding python: activity_streaks")
+
+def select_activity():
+    choice = input("choose activity?")
+
+showmenu()
+select_activity()
+
