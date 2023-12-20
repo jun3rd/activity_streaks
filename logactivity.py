e90dc554ce7d836705ae0baab124b7611c0bf014
alvarado <alvarado@Khaias-MacBook-Air.local>
2023-12-19 17:35:36 -0800

    ERROR: variable last_line in menu.py not testing string

--- logactivity.py
+++ logactivity.py
@@ -1,23 +1,36 @@
 import datetime
+import os
+
+currentfile = "activities.csv"
+
+def getFileLength():
+    path = currentfile
+    size = os.path.getsize(path)
+    return size
+
+def getLastLine():
+    f = open(currentfile, "r")
+    last_line = f.readlines()[-2]
+    return last_line
 
 def readlog():
-    f = open("activites.csv", "r")
+    f = open(currentfile, "r")
     print(f.read())
 
-def logstart():
+def python_logstart():
     x = datetime.datetime.now()
     x = str(x)
-    f = open("activities.csv", "a")
-    f.write("start\n")
+    f = open(currentfile, "a")
+    f.write("START\n")
     f.write(x)
     f.write("\n")
     f.close()
 
-def logend():
+def python_logstop():
     x = datetime.datetime.now()
     x = str(x)
-    f = open("activities.csv", "a")
-    f.write("end\n")
+    f = open(currentfile, "a")
+    f.write("STOP\n")
     f.write(x)
     f.write("\n")
     f.close()
--- menu.py
+++ menu.py
@@ -1,25 +1,55 @@
 import logactivity
 
 choice = 0
+filelength = 0
+last_line = ""
 
-def showmenu():
+def getFileLength():
+    global filelength
+    filelength = logactivity.getFileLength()
+    print("filelength: ", filelength)
+
+def getLastLine():
+    global last_line
+    last_line = logactivity.getLastLine()
+    print("last_line: ", last_line)
+
+def showMenu():
     print("Activities:")
     print("(1) START coding python: activity_streaks")
-    print("(2) END coding python: activity_streaks")
+    print("(2) STOP coding python: activity_streaks")
 
-def select_activity():
+def selectActivity():
     global choice
     choice = input("choose activity?")
     choice = int(choice)
+    global last_line
+    if (choice == 1 and last_line == "STOP"):
+            logactivity.python_logstart()
+    elif (choice == 2 and last_line == "START"):
+            logactivity.python_logstop()
+    else:
+        print("not an option")
 
-showmenu()
-select_activity()
+def showActivities():
+    logactivity.readlog()
 
-if choice == 1:
-    logactivity.logstart()
-elif choice == 2:
-    logactivity.logend()
-else:
-    print("not an option")
+def selectActivityTRY():
+    global choice
+    choice = input("choose activity?")
+    choice = int(choice)
+    global last_line
+    last_line = str(last_line)
+    print("last_line: ", last_line)
+    print("last_line type: ", type(last_line))
+    print("'STOP' type: ", type("STOP"))
+    print("last_line == STOP: ", last_line == 'STOP')
+    print("'WORD' == 'WORD': ", 'WORD' == 'WORD')
 
+
+showMenu()
+getFileLength()
+getLastLine()
+selectActivityTRY()
+showActivities()
 
